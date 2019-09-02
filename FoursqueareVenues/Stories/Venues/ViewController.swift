b7d67c1eab4e84b866be5daf6d0ed2082c38618b
Miguel Orjuela <miguel.orjuela@zemoga.com>
2019-09-02 15:39:14 -0500

    Initial commit

---
+++ FoursqueareVenues/Stories/Venues/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FoursqueareVenues
+//
+//  Created by Miguel Orjuela on 9/2/19.
+//  Copyright © 2019 Miguel Orjuela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
